0c443358b1136a9a20d81f29b1329a2516002a1f
goliot <cktnals2000@naver.com>
2024-10-23 15:08:33 +0900

    1023

---
+++ Sources/5427.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+int testcase, w, h;
+char map[1001][1001];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+queue<pair<int, int> > fire;
+queue<pair<int, int> > player;
+
+void moveFire() {
+    int cnt = (int)fire.size();
+    for(int i=0; i<cnt; i++) {
+        pair<int, int> cur = fire.front();
+        fire.pop();
+        for(int j=0; j<4; j++) {
+            int nx = dx[j] + cur.first;
+            int ny = dy[j] + cur.second;
+
+            if(nx < 0 || ny < 0 || nx >= h || ny >= w) continue;
+            if(map[nx][ny] != '.') continue;
+
+            map[nx][ny] = '*';
+            fire.emplace(nx, ny);
+        }
+    }
+}
+
+int movePlayer() {
+    int time = 0;
+    while(!player.empty()) {
+        time++;
+        moveFire();
+        int cnt = (int)player.size();
+        for(int i=0; i<cnt; i++) {
+            pair<int, int> cur = player.front();
+            player.pop();
+            for(int j=0; j<4; j++) {
+                int nx = dx[j] + cur.first;
+                int ny = dy[j] + cur.second;
+                if(nx < 0 || ny < 0 || nx >= h || ny >= w)
+                    return time;
+                if(map[nx][ny] != '.') continue;
+
+                map[nx][ny] = '@';
+                player.emplace(nx, ny);
+            }
+        }
+    }
+
+    return -1;
+}
+
+void reset() {
+    while(!fire.empty()) fire.pop();
+    while(!player.empty()) player.pop();
+    for(int i=0; i<h; i++) {
+        for(int j=0; j<w; j++) {
+            map[i][j] = '.';
+        }
+    }
+}
+
+int main() {
+    cin >> testcase;
+    while(testcase--) {
+        cin >> w >> h;
+        for(int i=0; i<h; i++) {
+            for(int j=0; j<w; j++) {
+                cin >> map[i][j];
+                if(map[i][j] == '*') fire.emplace(i, j);
+                else if(map[i][j] == '@') player.emplace(i, j);
+                else continue;
+            }
+        }
+
+        int res = movePlayer();
+        if(res == -1) cout << "IMPOSSIBLE" << '\n';
+        else cout << res << '\n';
+
+        reset();
+    }
+
+    return 0;
+}
