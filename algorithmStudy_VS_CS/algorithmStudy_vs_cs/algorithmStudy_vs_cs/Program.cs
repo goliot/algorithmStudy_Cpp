e5edb6ac8201ebe32b3b735b3c019de03093b4c3
Sumin Cha <109404269+goliot@users.noreply.github.com>
2025-03-05 13:39:58 +0900

    250305_add_vs_setting

---
+++ algorithmStudy_VS_CS/algorithmStudy_vs_cs/algorithmStudy_vs_cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 백준
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+        }
+    }
+}
